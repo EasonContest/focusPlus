6ceab35b8f3c71c2b9d0ea2a092b7e16ec1528de
darkfanxing <darkfanxing@gmail.com>
2020-07-25 04:55:28 +0800

    0455

--- app.js
+++ app.js
@@ -1,6 +1,4 @@
-console.log("QQ")
-
-const cookies = document.cookie
+// const cookies = document.cookie
 
 // 讀取API
 
@@ -15,59 +13,59 @@ const cookies = document.cookie
 //             })
 
 
-
-$(document).ready(function(){
-    $("#first_box").click(function() {
-        url = "http://localhost:13523/startRecord"
-        $.get(url, function(data,status){
-            alert("已經開始紀錄了喔!!");
-        });
+$("#first_box").click(() => {
+    console.log(123);
+    url = "http://localhost:13523/startRecord";
+    $.get(url, {user_id: ""}, function(response, status){
+        alert(response);
+        window.location.assign("./watching.html");
+        // alert("已經開始紀錄了喔!!");
     });
 });
 
-$(document).ready(function(){
-    $("#second_box").click(function() {
-        url = "http://localhost:13523/test"
-        $.get(url, function(data,status){
-            alert("數據：" + data + "\n狀態：" + status);
-        });
-    });
-});
+// $(document).ready(function(){
+//     $("#second_box").click(function() {
+//         url = "http://localhost:13523/test"
+//         $.get(url, function(data,status){
+//             alert("數據：" + data + "\n狀態：" + status);
+//         });
+//     });
+// });
 
-$(document).ready(function(){
-    $("#third_box").click(function() {
-        url = "http://localhost:13523/test"
-        $.get(url, function(data,status){
-            alert("數據：" + data + "\n狀態：" + status);
-        });
-    });
-});
+// $(document).ready(function(){
+//     $("#third_box").click(function() {
+//         url = "http://localhost:13523/test"
+//         $.get(url, function(data,status){
+//             alert("數據：" + data + "\n狀態：" + status);
+//         });
+//     });
+// });
 
-$(document).ready(function(){
-    $("#fourth_box").click(function() {
-        url = "http://localhost:13523/test"
-        $.get(url, function(data,status){
-            alert("數據：" + data + "\n狀態：" + status);
-        });
-    });
-});
+// $(document).ready(function(){
+//     $("#fourth_box").click(function() {
+//         url = "http://localhost:13523/test"
+//         $.get(url, function(data,status){
+//             alert("數據：" + data + "\n狀態：" + status);
+//         });
+//     });
+// });
 
-$(document).ready(function(){
-    $("#fifth_box").click(function() {
-        url = "http://localhost:13523/test"
-        $.get(url, function(data,status){
-            alert("數據：" + data + "\n狀態：" + status);
-        });
-    });
-});
+// $(document).ready(function(){
+//     $("#fifth_box").click(function() {
+//         url = "http://localhost:13523/test"
+//         $.get(url, function(data,status){
+//             alert("數據：" + data + "\n狀態：" + status);
+//         });
+//     });
+// });
 
-$(document).ready(function(){
-    $("#sixth_box").click(function() {
-        url = "http://localhost:13523/test"
-        $.get(url, function(data,status){
-            alert("數據：" + data + "\n狀態：" + status);
-        });
-    });
-});
+// $(document).ready(function(){
+//     $("#sixth_box").click(function() {
+//         url = "http://localhost:13523/test"
+//         $.get(url, function(data,status){
+//             alert("數據：" + data + "\n狀態：" + status);
+//         });
+//     });
+// });
 
 
