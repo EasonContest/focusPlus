c714d85fc1e1d3f68dddd385d05d98817e8cf139
janice880624 <47550853+janice880624@users.noreply.github.com>
2020-07-26 04:54:53 +0800

    history

--- history.js
+++ history.js
@@ -1,11 +1,15 @@
-var json = [{ "name": "小傑", "age": "05:06" },
-            { "name": "小明", "age": "04:10" }];
-var forTable = document.querySelector('.for-table tbody');
- 
-for (var i = 0, len = json.length; i < len; i++) {
-    forTable.innerHTML += 
-        '<tr>' +
-            '<td>' + json[i].name + '</td>' +
-            '<td>' + json[i].age + '</td>' +
-        '</tr>';
-}
+var json = []
+url = "http://localhost:13523/get_history";
+$.get(url, function(response){
+    json=response
+    console.log(Object.keys(json).length);
+}).then((val) => {
+    var forTable = document.querySelector('.for-table tbody');
+    for (var i = 0, len = Object.keys(json).length; i < len; i++) {
+        forTable.innerHTML += 
+            '<tr>' +
+                '<td>' + json[Object.keys(json)[i]]["name"] + '</td>' +
+                '<td>' + json[Object.keys(json)[i]]["time"] + '</td>' +
+            '</tr>';
+    }
+})
